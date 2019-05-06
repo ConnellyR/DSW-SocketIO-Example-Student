9a1ca9d61eeef6c298d6bb0bf7dfc3d6c436d121
Connelly R <42652898+ConnellyR@users.noreply.github.com>
2019-05-06 11:12:51 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -13,7 +13,20 @@ thread_lock = Lock() #we'll use this lock to prevent multiple clients from modif
 
 @app.route('/')
 def index():
+   
     return render_template('home.html', async_mode=socketio.async_mode)
+
+
+
+@socket.on('connect')
+def test_connet():
+    global thread # using the thread var created above. 
+    with thread_lock: # lock the thread so other clients can't change it.
+        if thread is None:
+            thread=sockio.start_background_task(target=background_through)
+    emit('start''connected',)#name of event. send message to client
+
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
